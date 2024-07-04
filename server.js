c729adcf00eae41ca5ffdc6f8ca097b887f9fb62
thisisrekha <kondurinaga@iitbhilai.ac.in>
2024-07-04 13:25:52 +0530

    add cart page and auth

--- server.js
+++ server.js
@@ -6,16 +6,28 @@ const expressLayout = require ("express-ejs-layouts")
 const PORT= process.env.PORT || 3000
 
 //Assets
-app.use(express.static("public"))
-app.get("/",function(req,res){
-    res.render("home")
-})
 
 
 //set template engine
 app.use(expressLayout)
 app.set("views", path.join(__dirname, '/resources/views'))
 app.set("view engine","ejs")
+
+
+app.use(express.static("public"))
+app.get("/",(req,res) =>{
+    res.render("home")
+})
+
+app.get("/cart",(req,res) =>{
+    res.render("customers/cart")
+})
+app.get("/login",(req,res) =>{
+    res.render("auth/login")
+})
+app.get("/register",(req,res) =>{
+    res.render("auth/register")
+})
 
 
 
